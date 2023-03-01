17052d02d62d1de0a250d53831773c782d54e852
kitrath <crathmel@gmail.com>
2023-03-01 17:17:08 -0600

    add note delete

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,6 @@
 const notes = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-const { readFromFile, appendToFile } = require('../utils/fs-utils');
+const { readFromFile, writeToFile, appendToFile } = require('../utils/fs-utils');
 
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json')
@@ -30,6 +30,40 @@ notes.post('/', (req, res) => {
     } else {
         res.json('Error in creating note');
     }
+});
+
+notes.delete('/:id', (req, res) => {
+
+    const id = req.params.id;
+
+    readFromFile('./db/db.json').then((data) => {
+
+        let removed = false;
+        const currentList = JSON.parse(data);
+
+        const newList = currentList.filter((item) => {
+            if (item.id === id) {
+                removed = true;
+                return false;
+            }
+            return true;
+        });
+
+        writeToFile('./db/db.json', newList);
+
+        if (removed) {
+            res.json({
+                status: 'success',
+                body: `note ${id} removed from db.json`
+            });
+        } else {
+            res.status(404).json({
+                status: 'Not Found',
+                body: `note ${id} not found`
+            });
+        }
+    })
+    .catch((err) => console.error(err))
 });
 
 module.exports = notes;
